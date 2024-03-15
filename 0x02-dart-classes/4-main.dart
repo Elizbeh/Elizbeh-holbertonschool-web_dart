4f9eb49a5a9dcc59ef8b59440c9a0559b683d442
root <root@LAPTOP-J7HTNKBB>
2024-03-16 00:30:00 +0100

    4. Mutables

---
+++ 0x02-dart-classes/4-main.dart
@@ -0,0 +1,9 @@
+import '4-mutables.dart';
+void main() {
+  final password = Password(password: "Passwordecode");
+  print(password.toString());
+  print(password.isValid());
+  password.password = "Youssef4321";
+  print(password.toString());
+  print(password.isValid());
+}
---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,28 @@
+class Password {
+  String _password = '';
+
+  Password({required String password}) : _password = password;
+
+    bool isValid() {
+    if (_password.length < 8 || _password.length > 16) return false;
+
+    if (!RegExp(r'[A-Z]').hasMatch(_password)) return false;
+    if (!RegExp(r'[a-z]').hasMatch(_password)) return false;
+    if (!RegExp(r'[0-9]').hasMatch(_password)) return false;
+
+    return true;
+  }
+
+  String get password{
+    return _password;
+  }
+
+  set password(String value) {
+    _password = value;
+  }
+
+  @override 
+  String toString() {
+    return 'Password $_password';
+  }
+}
