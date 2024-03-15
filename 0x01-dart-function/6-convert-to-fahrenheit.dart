016647de7b7bf39d14f238931293565e88c2dd62
root <root@LAPTOP-J7HTNKBB>
2024-03-15 11:01:43 +0100

    Modify function to get the correct output

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,4 +1,5 @@
-List<String> convertToF(List<double> temperaturesInC) {
-  return temperaturesInC
-  .map((celsius) => ((celsius * 9 / 5) + 32).toStringAsFixed(2)).toList();
+List<double> convertToF(List<double> temperaturesInC) {
+    return temperaturesInC.map((double celsius) {
+        return double.parse(((celsius * 9/5) + 32).toStringAsFixed(2));
+    }).toList();
 }
