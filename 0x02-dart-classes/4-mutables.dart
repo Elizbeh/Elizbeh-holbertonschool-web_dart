70c271f1bd21556ef08dad0e49cedb2b482d89de
root <root@LAPTOP-J7HTNKBB>
2024-03-16 22:59:56 +0100

    .....

--- 0x02-dart-classes/4-mutables.dart
+++ 0x02-dart-classes/4-mutables.dart
@@ -4,14 +4,14 @@ class Password {
   Password({required String password}) : _password = password;
 
   bool isValid() {
-    if (_password.length < 8 || _password.length > 16) return false;
-
-    if (!RegExp(r'[A-Z]').hasMatch(_password)) return false;
-    if (!RegExp(r'[a-z]').hasMatch(_password)) return false;
-    if (!RegExp(r'[0-9]').hasMatch(_password)) return false;
-
-    return true;
+  if (_password.length < 8 || _password.length > 16) {
+    return false;
   }
+  
+  final pattern = RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])');
+  return pattern.hasMatch(_password);
+}
+
 
   String get password => _password;
 
