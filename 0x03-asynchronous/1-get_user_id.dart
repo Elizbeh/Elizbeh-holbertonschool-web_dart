107b56e6f86731a34ff703c39620dfc478752277
root <root@LAPTOP-J7HTNKBB>
2024-03-18 23:47:59 +0100

    1. Get User ID

---
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -0,0 +1,10 @@
+import 'dart:convert';
+import '1-util.dart';
+
+Future<String> getUserId() async {
+  String userDataJson = await fetchUserData();
+  
+  Map<String, dynamic> userData = json.decode(userDataJson);
+  String userId = userData['id'];
+  return userId;
+}
