2f97be38fb42438f77196a4da3be9866d5d9ba0a
root <root@LAPTOP-J7HTNKBB>
2024-03-19 01:17:27 +0100

    4. Calcul Total

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import '4-util.dart';
+
+
+Future<double> calculateTotal() async{
+  try {
+    var userDataJson = await fetchUserData();
+    var userData = json.decode(userDataJson);
+    var userId = userData['id']; 
+    // fetch user orders
+    var userOrdersJson = await fetchUserOrders(userId);
+    var userOrders = json.decode(userOrdersJson);
+
+    double totalPrice = 0;
+    for (var product in userOrders) {
+      var productPriceJson = await fetchProductPrice(product);
+      var productPrice = json.decode(productPriceJson);
+      totalPrice += double.parse(productPrice.toString());
+    }
+    return totalPrice;
+  } catch (error) {
+    return -1; 
+  }
+}
